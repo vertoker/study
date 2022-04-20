59df3602a8dc6ce1eec5a397e7a095ded55a76ad
vertoker <vertogpro@gmail.com>
2022-04-20 17:16:30 +0500

    do nothing

--- ClientAuthorization_Чураков/Form1.cs
+++ ClientAuthorization_Чураков/Form1.cs
@@ -6,5 +6,10 @@ namespace ClientAuthorization_Чураков
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
