bed44eec46de80d867397e006b4786aa40ce0c75
vertoker <vertogpro@gmail.com>
2022-09-03 07:08:59 +0300

    add pages and several pages

--- churakov_project/churakov_project/settings.py
+++ churakov_project/churakov_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'churakov_app'
+    'churakov_app',
+    'churakov_pages'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'churakov_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'churakov_project.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
--- churakov_project/churakov_project/urls.py
+++ churakov_project/churakov_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from churakov_app import views
+#from churakov_app import views1
+from churakov_pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homePageView)
+    path('about/', views.aboutPage),
+    path('', views.homePage)
 ]
