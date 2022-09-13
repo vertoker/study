336f3fd01be722fbafecc838ff78160044e6392d
vertoker <vertogpro@gmail.com>
2022-09-13 13:28:04 +0300

    complete task 7

--- churakov_users/users/models.py
+++ churakov_users/users/models.py
@@ -4,8 +4,5 @@ from django.db import models
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(default=0)
 
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
-
     def __str__(self):
         return self.title
