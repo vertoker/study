2fb18e7574d3dc176f2bf5dceacdc6e5f2dee08a
vertoker <vertogpro@gmail.com>
2022-10-29 17:06:10 +0500

    add haircut 2

--- barbershop/haircuts/models.py
+++ barbershop/haircuts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.postgres.fields import ArrayField
+from django import forms
 
 class Haircut(models.Model):
     title = models.CharField(max_length=200)
-    image = models.URLField(max_length=200)
-    description = models.TextField()
+
+    image1 = models.URLField(max_length=200, default=None)
+    image2 = models.URLField(max_length=200, default=None)
+    image3 = models.URLField(max_length=200, default=None)
+    image4 = models.URLField(max_length=200, default=None)
+    image5 = models.URLField(max_length=200, default=None)
+
+    description = models.TextField(default=None)
+    full_description = models.TextField(default=None)
+    price = models.PositiveIntegerField()
+    old_price = models.PositiveIntegerField(default=None)
 
     def get_absolute_url(self):
         return reverse('haircut_detail', args=[str(self.id)])
--- barbershop/haircuts/views.py
+++ barbershop/haircuts/views.py
@@ -14,12 +14,12 @@ class HaircutDetailView(DetailView):
 class HaircutCreateView(CreateView):
     model = Haircut
     template_name = 'haircut_new.html'
-    fields = ['title', 'image', 'description']
+    fields = ['title', 'image1', 'image2', 'image3', 'image4', 'image5', 'description', 'full_description', 'price', 'old_price']
 
 class HaircutUpdateView(UpdateView):
     model = Haircut
     template_name = 'haircut_edit.html'
-    fields = ['title', 'image', 'description']
+    fields = ['title', 'image1', 'image2', 'image3', 'image4', 'image5', 'description', 'full_description', 'price', 'old_price']
 
 class HaircutDeleteView(DeleteView):
     model = Haircut
