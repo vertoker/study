8e9d7ddd9169b280dd014423a3474728ff607d15
vertoker <vertogpro@gmail.com>
2022-11-04 20:23:34 +0500

    update

--- barbershop/appointments/models.py
+++ barbershop/appointments/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django import forms
 
 class Appointment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     haircut = models.ForeignKey("haircuts.haircut", on_delete=models.PROTECT, null=True)
-    user = models.ForeignKey("users.CustomUser", on_delete=models.PROTECT, null=True)
     master = models.ForeignKey("masters.Master", on_delete=models.PROTECT, null=True)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
 
